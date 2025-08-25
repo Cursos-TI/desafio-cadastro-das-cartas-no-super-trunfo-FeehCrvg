8cbe600534b4914fa026c139dd2436a6f64826cf
FeehCrvg <fevascopaula@gmail.com>
2025-08-25 01:02:34 +0000

    Programa finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -24,3 +24,17 @@ printf("A Área em km² é: 1.480.886 Km² \n");
 printf("O PIB é: 190.84 Bilhões de reais \n");
 printf("O Número de Pontos Turistícos é: 50 \n");
 
+printf("\n");
+
+printf("Carta 2 \n");
+printf("O Estado é: Bahia \n");
+printf("O Código é: B02 \n");
+printf("A Cidade é: Salvador \n");
+printf("A População é: 94100000 \n");
+printf("A Área em km² é: 5.640.830 Km² \n");
+printf("O PIB é: 230.65 Bilhões de reais \n");
+printf("O Número de Pontos Turistícos é: 63 \n");
+
+return 0 ;
+
+}
